59c55f1dc8f461d8716d1385ed21d946e4225a56
Shubhraj Prasad Singh <shubhraj@loyalie.com>
2022-02-12 01:08:41 +0530

    Frontend UI expect tables

--- src/components/Elements/SearchDialog/SearchDialog.jsx
+++ src/components/Elements/SearchDialog/SearchDialog.jsx
@@ -5,8 +5,24 @@ export default function SearchDialog(){
         <div className="p-12 bg-red-100 rounded-3xl shadow-lg">
             <h3 className="font-bold text-3xl">Find the best Shipping Deals</h3>
             <div className="grid grid-cols-2 grid-rows-1 space-x-8">
-                <input type="text" class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Source" />
-                <input type="text" class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Destination" />
+                <select
+                    class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
+                    placeholder="Source">
+                        <option value="grapefruit">Dhanbad</option>
+                        <option value="lime">Patna</option>
+                        <option selected value="coconut">Bangalore</option>
+                        <option value="mango">Delhi</option>
+                </select>
+                <select
+                    class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
+                    placeholder="Destination">
+                        <option value="grapefruit">Dhanbad</option>
+                        <option value="lime">Patna</option>
+                        <option selected value="coconut">Bangalore</option>
+                        <option value="mango">Delhi</option>
+                </select>
+                {/* <input type="text" class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Source" /> */}
+                {/* <input type="text" class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Destination" /> */}
             </div>
             <div className="grid grid-cols-2 grid-rows-1 space-x-8">
                 <input type="number" class="mt-8 form-control px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Weight" />
--- src/components/VendorProfle/MultiSelect/MultiSelect.jsx
+++ src/components/VendorProfle/MultiSelect/MultiSelect.jsx
@@ -24,6 +24,7 @@ export default function MultiSelect(){
         <div className="p-4 rounded-xl shadow-md my-8 bg-white">
             <h4 className='text-base font-medium text-left mb-4 text-gray-600'>Operating Cities</h4>
             <Select 
+                isMulti 
                 options={Countries} 
                 defaultValue={selected} 
                 components={animatedComponents}
